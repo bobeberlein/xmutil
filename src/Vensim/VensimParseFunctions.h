43386ef3280971660d61d0c5cc1d95c63bd4e125
Bob Eberlein <bob@iseesystems.com>
2018-01-18 14:20:49 -0500

    pulled out remnant extern "C" marker

--- src/Vensim/VensimParseFunctions.h
+++ src/Vensim/VensimParseFunctions.h
@@ -1,35 +1,30 @@
 #ifndef _XMUTIL_VENSIM_VENSIMPARSEFUNCTIONS_H
 #define _XMUTIL_VENSIM_VENSIMPARSEFUNCTIONS_H
 #include "../Symbol/Parse.h"
-#ifdef __cplusplus
-#define CNF "C"
-#else
-#define CNF 
-#endif
 
-extern CNF void vpyy_addfulleq(Equation *eq,UnitExpression *un) ;
-extern CNF Equation *vpyy_addeq(LeftHandSide *lhs,Expression *ex,ExpressionList *exl,int token) ;
-extern CNF Equation *vpyy_add_lookup(LeftHandSide *lhs, Expression *ex, ExpressionTable *tvl, int legacy);
-extern CNF LeftHandSide *vpyy_addexceptinterp(ExpressionVariable *var, SymbolListList *except, int interpmode);
-extern CNF SymbolList *vpyy_symlist(SymbolList *in,Variable *add,int bang,Variable *end) ;
-extern CNF SymbolList *vpyy_mapsymlist(SymbolList* in,Variable *maprange, SymbolList *list);
-extern CNF UnitExpression *vpyy_unitsdiv(UnitExpression *num, UnitExpression *denom);
-extern CNF UnitExpression *vpyy_unitsmult(UnitExpression *f,UnitExpression *s) ;
-extern CNF UnitExpression *vpyy_unitsrange(UnitExpression *f,double minval,double maxval,double increment) ;
-extern CNF SymbolListList *vpyy_chain_sublist(SymbolListList *sll,SymbolList *nsl) ;
-extern CNF ExpressionList *vpyy_chain_exprlist(ExpressionList *el,Expression *e) ;
-extern CNF Expression *vpyy_num_expression(double num) ;
-extern CNF Expression *vpyy_literal_expression(const char *tok);
-extern CNF ExpressionVariable *vpyy_var_expression(Variable *var,SymbolList *subs) ;
-extern CNF ExpressionSymbolList *vpyy_symlist_expression(SymbolList *subs,SymbolList *map) ;
-extern CNF Expression *vpyy_operator_expression(int oper,Expression *exp1,Expression *exp2) ;
-extern CNF Expression *vpyy_function_expression(Function *func,ExpressionList *args) ;
-extern CNF Expression *vpyy_lookup_expression(ExpressionVariable *var,Expression *exp) ;
-extern CNF ExpressionTable *vpyy_tablepair(ExpressionTable *table,double x,double y) ;
-extern CNF ExpressionTable *vpyy_tablevec(ExpressionTable *table, double val);
-extern CNF ExpressionTable *vpyy_tablerange(ExpressionTable *table, double x1, double y1, double x2, double y2);
-extern CNF void vpyy_macro_start();
-extern CNF void vpyy_macro_expression(Variable *name, ExpressionList *margs);
-extern CNF void vpyy_macro_end();
+extern void vpyy_addfulleq(Equation *eq,UnitExpression *un) ;
+extern Equation *vpyy_addeq(LeftHandSide *lhs,Expression *ex,ExpressionList *exl,int token) ;
+extern Equation *vpyy_add_lookup(LeftHandSide *lhs, Expression *ex, ExpressionTable *tvl, int legacy);
+extern LeftHandSide *vpyy_addexceptinterp(ExpressionVariable *var, SymbolListList *except, int interpmode);
+extern SymbolList *vpyy_symlist(SymbolList *in,Variable *add,int bang,Variable *end) ;
+extern SymbolList *vpyy_mapsymlist(SymbolList* in,Variable *maprange, SymbolList *list);
+extern UnitExpression *vpyy_unitsdiv(UnitExpression *num, UnitExpression *denom);
+extern UnitExpression *vpyy_unitsmult(UnitExpression *f,UnitExpression *s) ;
+extern UnitExpression *vpyy_unitsrange(UnitExpression *f,double minval,double maxval,double increment) ;
+extern SymbolListList *vpyy_chain_sublist(SymbolListList *sll,SymbolList *nsl) ;
+extern ExpressionList *vpyy_chain_exprlist(ExpressionList *el,Expression *e) ;
+extern Expression *vpyy_num_expression(double num) ;
+extern Expression *vpyy_literal_expression(const char *tok);
+extern ExpressionVariable *vpyy_var_expression(Variable *var,SymbolList *subs) ;
+extern ExpressionSymbolList *vpyy_symlist_expression(SymbolList *subs,SymbolList *map) ;
+extern Expression *vpyy_operator_expression(int oper,Expression *exp1,Expression *exp2) ;
+extern Expression *vpyy_function_expression(Function *func,ExpressionList *args) ;
+extern Expression *vpyy_lookup_expression(ExpressionVariable *var,Expression *exp) ;
+extern ExpressionTable *vpyy_tablepair(ExpressionTable *table,double x,double y) ;
+extern ExpressionTable *vpyy_tablevec(ExpressionTable *table, double val);
+extern ExpressionTable *vpyy_tablerange(ExpressionTable *table, double x1, double y1, double x2, double y2);
+extern void vpyy_macro_start();
+extern void vpyy_macro_expression(Variable *name, ExpressionList *margs);
+extern void vpyy_macro_end();
 
 #endif
