c7100d178cae7d948ad7527649de3ec3b1521191
Bobby Powers <bobbypowers@gmail.com>
2021-10-26 22:05:09 -0700

    build/environment.sh: ensure directory third_party/include exists

--- build/environment.sh
+++ build/environment.sh
@@ -3,9 +3,7 @@ if uname -a | grep -q 'MINGW'; then
 	#windows  
 	export GYP_MSVS_VERSION='2017'
 	export QTDIR="C:/Qt/x64/Qt5.11.0/5.11.0/msvc2017_64"
-else
-
-if  uname -a | grep -q 'Linux'; then
+elif  uname -a | grep -q 'Linux'; then
 	export QTDIR="$HOME/Qt5.11.0/5.11.0/gcc_64"
 else
 	#mac
@@ -15,11 +13,11 @@ else
     	export QTDIR="$HOME/Qt5.15.4/5.15.4/clang_arm64"
     fi
 
-    rm -f ./third_party/include/QtGui
+	rm -f ./third_party/include/QtGui
+	mkdir -p third_party/include
 	ln -s -f $QTDIR/lib/QtGui.framework/Headers ./third_party/include/QtGui
-    rm -f ./third_party/include/QtCore
+	rm -f ./third_party/include/QtCore
 	ln -s -f $QTDIR/lib/QtCore.framework/Headers ./third_party/include/QtCore
-    rm -f ./third_party/include/QtWidgets
+	rm -f ./third_party/include/QtWidgets
 	ln -s -f $QTDIR/lib/QtWidgets.framework/Headers ./third_party/include/QtWidgets
-fi
 fi
